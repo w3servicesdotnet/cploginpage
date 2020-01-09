f66ad1aba81dd841a7e924cc4bbe44a549e7d202
W3SERVICES <32176465+w3servicesdotnet@users.noreply.github.com>
2020-01-09 20:07:30 +0530

    Update home.sh

--- home.sh
+++ home.sh
@@ -1,1 +1,5 @@
+#!/bin/bash
 
+GITRAW=https://raw.githubusercontent.com/w3servicesdotnet/cploginpage
+LSWSDIR=/usr/local/CyberCP/loginSystem/templates/loginSystem
+wget -O $LSWSDIR/login.html $GITRAW/login.html
